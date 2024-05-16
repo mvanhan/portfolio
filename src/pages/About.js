50a2cded1576a49c7560aeeed5805ca1bdc7ce3c
Miller Van Hanken <mvanhan@unc.edu>
2024-05-16 14:43:13 -0500

    About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div className="main-content">
-      <h1>About Me</h1>
-      <p>Hi, I'm Walter Miller Van Hanken. I'm a 3rd-year student at UNC Chapel Hill, currently residing in Tulsa, Oklahoma. Feel free to reach out to me at mvanhanken@gmail.com or call me at (918) 703-3547.</p>
-      <p>Here you can add more details about your background, interests, and other relevant information.</p>
-      <h2>My Resume</h2>
-      <iframe 
-        src="/WMV Resume.pdf" 
-        title="My Resume" 
-        style={{ width: '50%', height: '600px', border: 'none' }}>
-      </iframe>
+    <div className="pdf-container">
+      <div className="pdf-text-box">
+        <h1>About Me</h1>
+        <p>Hi, I'm Walter Miller Van Hanken. I'm a 3rd-year student at UNC Chapel Hill, currently residing in Tulsa, Oklahoma. Feel free to reach out to me at mvanhanken@gmail.com or call me at (918) 703-3547.</p>
+        <p>Here you can add more details about your background, interests, and other relevant information.</p>
+      </div>
+      <div className="pdf-viewer">
+        <h2 style={{textAlign: 'center'}}>My Resume</h2>
+        <iframe 
+          src="/WMV Resume.pdf" 
+          title="My Resume" 
+          style={{ width: '100%', height: '600px', border: 'none' }}>
+        </iframe>
+      </div>
     </div>
   );
 };
 
 export default About;
-
-
-
