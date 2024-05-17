2d03e0297446e9802007c59cbef8d61c186cd327
Miller Van Hanken <mvanhan@unc.edu>
2024-05-17 16:01:25 -0500

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,11 @@ const Home = () => {
       <Typed
         strings={[
           "Hi, I'm Miller. Welcome to my portfolio!",
-          " ",
-          "I am 3rd-year student at UNC Chapel Hill, studying computer science and philosophy. I am passionate about new technologies, but I am particularly interested in the intersection of technology and ethics.",
           "Please check out my projects and more about me!",
         ]}
-        typeSpeed={20}
-        // backSpeed={50}
-        // backDelay={2000}
+        typeSpeed={50}
+        backSpeed={20}
+        backDelay={2000}
         // loop
       />
     </div>
